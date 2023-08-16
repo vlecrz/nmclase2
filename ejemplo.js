20059693e64a6d88dcd8e056f22c68865a253ef7
Valentina Cruz <141838196+vlecrz@users.noreply.github.com>
2023-08-16 18:35:30 -0400

    Add files via upload

---
+++ ejemplo.js
@@ -0,0 +1,21 @@
+[{
+        "name": " Diseño y Nuevas Tendencias de la Audiovisualidad Contemporánea",
+        "code": "DNO031",
+        "about": "En la contemporaneidad se presencian situaciones liberadas a la instantaneidad y simultaneidad, a la desestabilización de las categorías establecidas de sentido narrativo, verdades fractales, indeterminación Elementos que inciden en la construcción de realidad, de memoria y de identidad En este escenario y considerando que el imaginario visual y audiovisual no puede pensarse al margen de los procesos tecnológicos y culturales de la época, el curso abre un modelo de análisis y pensamiento crítico para el ámbito de la representación audiovisual contemporánea, distinguiendo y reflexionando sobre las nuevas tendencias que se ponen en juego en la puesta obra actual."
+    },
+    {
+        "name": "Laboratorio de Experimentación y Prototipado. (Antiguo Laboratorio de modelos y prototipos)",
+        "code": "DNO1032",
+        "about": "Curso teórico-práctico que permite al estudiante aplicar el prototipado como una herramienta integral en el proceso de diseño, poniendo en práctica habilidades para tangibilizar ideas e incorporar información a través del uso de prototipos, incorporando como base la sustentabilidad y como objetivo final el lograr un nivel profesional en la fabricación. El estudiante formará una visión estratégica respecto al uso de prototipos en el proceso de desarrollo, incorporando la validación y la iteración como elementos clave. Se realizarán ejercicios prácticos grupales e individuales de carácter diagnóstico, formativo y sumativo, junto con el desarrollo de proyectos."
+    },
+    {
+        "name": "Pensar la Artesanía. Bases para la Comprensión y Acción (Antiguo con la profesora Celina)",
+        "code": "DNO044",
+        "about": "El curso abre un modelo de análisis y reflexión valorica para la relación entre el mundo profesional y académico con los artesanos poseedores de estos saberes, y nuevos campos de acción en la interacción entre estos dos ámbitos del conocimiento que potencian el desarrollo de ambos sectores en pos de un desarrollo humano sustentable."
+    },
+    {
+        "name": "Taller de Introducción al Proyecto de Diseño. (Antiguo Introducción al proyecto)",
+        "code": "DNO1011",
+        "about": "En este curso los estudiantes se iniciarán en la disciplina proyectual del diseño de manera integral, adquiriendo herramientas que le permitan comprender los contextos de diseño mediante la observación, definición y relación de variables. Desarrollando la sensibilidad para observar fenómenos y entornos, y la composición de formas abstractas, con un fuerte cuidado por el oficio y la proporción. A partir de la observación de los contextos, los estudiantes podrán conocer y comprender la cultura del diseño en su dimensión social, ambiental y cultural, para iniciar procesos explorativos abstractos, estableciendo un diálogo formal transversal que vincule la sustentabilidad medioambiental y social, con el bienestar de las personas y los ecosistemas."
+    }
+]
